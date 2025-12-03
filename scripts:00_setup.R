041cff12dda71ab60775b6bc1caf61dcf41de71e
Karen Mondragon <kmondragon.md@gmail.com>
2025-12-03 15:11:50 +0100

    Add setup script

---
+++ scripts:00_setup.R
@@ -0,0 +1,11 @@
+# scripts/00_setup.R
+
+# Load packages ----
+library(tidyverse)
+library(mlbench)
+library(here)
+library(janitor)
+library(gtsummary)
+
+# Check working directory (should be project root)
+here::here()
